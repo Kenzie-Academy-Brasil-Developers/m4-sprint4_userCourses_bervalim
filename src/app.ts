c89ed8b90646a7bbfd1f5bf22ed70cfb2cca60b6
bervalim <bernardogvalim@gmail.com>
2023-10-12 16:43:36 -0300

    feat/error_validation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import "express-async-errors";
 import "dotenv/config";
 import express, { Application, json } from "express";
+import { handleErrors } from "./middlewares/handleErrors.middleware";
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.use(handleErrors);
 
 export default app;
