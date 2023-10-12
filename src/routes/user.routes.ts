8f8542442665ef8c2c3096f4c66e06b1a9297a70
bervalim <bernardogvalim@gmail.com>
2023-10-12 18:05:55 -0300

    feat/validationBody

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import { createUserController } from "../controllers/user.controller";
+import { validateBody } from "../middlewares/validateBody.middleware";
+import { userCreateRequestSchema } from "../schemas/user.schema";
 
 export const userRouter: Router = Router();
 
-userRouter.post("/", createUserController);
+userRouter.post(
+  "/",
+  validateBody(userCreateRequestSchema),
+  createUserController
+);
