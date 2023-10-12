8e21d41fd98c7247224bc74c3fe1c3239870a4b4
bervalim <bernardogvalim@gmail.com>
2023-10-12 18:52:41 -0300

    feat/courses

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { userRouter } from "./user.routes";
+import { coursesRouter } from "./courses.routes";
 
 export const allRoutes: Router = Router();
 
 allRoutes.use("/users", userRouter);
+allRoutes.use("/courses", coursesRouter);
